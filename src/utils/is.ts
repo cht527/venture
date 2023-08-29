bc858e7dc24ac6a87a90fce3f8fcf12908d81c21
cht527 <cht527@126.com>
2023-08-29 22:22:26 +0800

    remove file

--- src/utils/is.ts
+++
@@ -1,6 +0,0 @@
-export const isUndefined = (value: unknown): value is undefined => value === undefined;
-
-export const isPromise = (obj: unknown): obj is PromiseLike<unknown> =>
-  !!obj &&
-  (typeof obj === 'object' || typeof obj === 'function') &&
-  typeof (obj as PromiseLike<unknown>).then === 'function';
